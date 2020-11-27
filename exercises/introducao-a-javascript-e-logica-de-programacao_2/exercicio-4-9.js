2994c4bb6d5b711a98b83260690547dfa04ac9ae
Renan Costa <renancosta21@gmail.com>
2020-11-26 23:54:02 -0300

    exercício 9 feito

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio-4-9.js
@@ -0,0 +1,7 @@
+let array = [];
+for (let index = 1; index <= 25; index += 1) {
+    array.push(index);
+}
+for (index = 0; index < array.length; index += 1) {
+    console.log(array[index] + '/2 = ' + array[index] / 2);
+}
